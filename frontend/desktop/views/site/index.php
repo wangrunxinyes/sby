c405422b4c029dca3ad41ccf80259346bf1ff2fd
wangrunxin <wangrunxinyes@gmail.com>
2016-03-17 20:58:28 +0800

    -update test

--- frontend/desktop/views/site/index.php
+++ frontend/desktop/views/site/index.php
@@ -121,14 +121,9 @@ Yii::app ()->assets->registerScript ( "frame.layout/js/custom.js" );
 				<div class="span4 offset1">
 					<div>
 						<h2>
-							We live with <strong>creativity</strong>
+							I live with <strong>creativity</strong>
 						</h2>
-						<p>Li Europan lingues es membres del sam familie. Lor separat
-							existentie es un myth. Por scientie, musica, sport etc, litot
-							Europa usa li sam vocabular. Li lingues differe solmen in li
-							grammatica, li pronunciation e li plu commun vocabules. Omnicos
-							directe al desirabilite de un nov lingua franca: On refusa
-							continuar payar custosi traductores.</p>
+						<p>what should I say here?</p>
 					</div>
 				</div>
 				<div class="span6">
